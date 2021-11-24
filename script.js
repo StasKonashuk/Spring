9b22bd1bab99317cdcd4173ff531b24381034d48
Stanislav <skonashuk@list.ru>
2021-11-24 16:53:52 +0300

    Change script for web page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
-  $('.header-burger').click(function(event){
-    $('.header-burger, .header-menu').toggleClass('active');
-    $('body').toggleClass('lock');
-  })
-});
+let header_burger = document.querySelector('.header-burger');
+let header_menu = document.querySelector('.header-menu');
+let body = document.querySelector('body');
+header_burger.onclick = function(){
+  header_burger.classList.toggle('active');
+  header_menu.classList.toggle('active');
+  body.classList.toggle('lock');
+}
