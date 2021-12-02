0ecc051a93e83d500ec26ef38f71a6e48f5dddf9
Stanislav <skonashuk@list.ru>
2021-12-02 18:49:25 +0300

    Add array of objects with information about nav items

--- constants/catalog.js
+++ constants/catalog.js
@@ -132,3 +132,67 @@ const PROJECTS = [
     text: 'Facilitates the development of contract-first SOAP web services.'
   }
 ]
+const NAV_ITEMS = [
+  {
+    id: 1,
+    title: 'Why Spring',
+    submenu: [
+      'Overview',
+      'Microservices',
+      'Reactive',
+      'Event Driven',
+      'Cloud',
+      'Web Applications',
+      'Serverless',
+      'Batch'
+    ]
+  },
+  {
+    id: 2,
+    title: 'Learn',
+    submenu: [
+      'Overview',
+      'Quickstart',
+      'Guides',
+      'Blog'
+    ]
+  },
+  {
+    id: 3,
+    title: 'Projects',
+    submenu: [
+      'Overview',
+      'Spring Boot',
+      'Spring Framework',
+      'Spring Cloud',
+      'Spring Cloud Data Flow',
+      'Spring Data',
+      'Spring Integration',
+      'Spring Batch',
+      'Spring Security',
+      'View all Projects',
+      'Development Tools',
+      'Spring Tools 4'
+    ]
+  },
+  {
+    id: 4,
+    title: 'Training',
+    submenu: []
+  },
+  {
+    id: 5,
+    title: 'Support',
+    submenu: []
+  },
+  {
+    id: 6,
+    title: 'Community',
+    submenu: [
+      'Overview',
+      'Events',
+      'Team'
+    ]
+  },
+  
+]
--- constants/root.js
+++ constants/root.js
@@ -1,1 +1,2 @@
 const ROOT_PROJECTS = document.getElementsByClassName('proj-content')
+const ROOT_NAV_ITEMS = document.getElementsByClassName('header-menu')
