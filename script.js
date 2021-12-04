cbb87fec8d7c9776089ab5b1828b375ef48cd52f
Stanislav <skonashuk@list.ru>
2021-12-04 19:24:59 +0300

    Add projects search

--- script.js
+++ script.js
@@ -49,3 +49,60 @@ if(iconMenu){
     menuBody.classList.toggle('_active')
   });
 }
+
+document.querySelector('.proj-input').oninput = function(){
+  setTimeout(() => {
+    let val = this.value.trim().toLowerCase();
+  let projects = PROJECTS;
+  let htmlCatalog = "";
+  if(val !== ''){
+    projects.forEach(({title, text, imageUrl}) => {
+      if(title.toLowerCase().indexOf(val) === -1 && text.toLowerCase().indexOf(val) === -1){
+        htmlCatalog += ``;
+      }else{
+        htmlCatalog += `
+        <a class="project-box">
+          <div class="proj-icon-box">
+            <img class="proj-icon" src=${imageUrl}/>
+          </div>
+          <div class="proj-text-conteiner">
+            <h3 class="proj-text-title">${title}</h3>
+            <p class="proj-text">${text}</p>
+          </div>
+        </a>
+      `;
+      }
+    })
+  }else{
+    PROJECTS.forEach(({ id, imageUrl, title, text }) => {
+      htmlCatalog += `
+        <a class="project-box">
+          <div class="proj-icon-box">
+            <img class="proj-icon" src=${imageUrl}/>
+          </div>
+          <div class="proj-text-conteiner">
+            <h3 class="proj-text-title">${title}</h3>
+            <p class="proj-text">${text}</p>
+          </div>
+        </a>
+      `;
+    });
+  }
+  if(htmlCatalog === ``){
+    htmlCatalog = `
+      <div>
+        <h1 class = 'no-results'>No Results</h1>
+      </div>
+    `
+  }
+  const html = `
+      <div class="project-box-container">
+        ${htmlCatalog}
+      </div>
+    `;
+  for (let i = 0; i < ROOT_PROJECTS.length; i++) {
+    ROOT_PROJECTS[i].innerHTML = html;
+  }
+  }, 300)
+}
+
